f776e6afa1f8a3f6695c203cea59091ae4a9fd8d
yiuuukl0321 <krystalyiuu0321@gmail.com>
2026-09-17 21:53:21 +0800

    Update fix.js

--- fix.js
+++ fix.js
@@ -1,8 +1,6 @@
 /* 咩&砚 · 补丁包
 
-/* 暗度归零 */
-if (+S.dim !== 0){ S.dim = 0; save(); }
-setTimeout(function(){ if (typeof applyWall === 'function') applyWall(); }, 400);
+
 
 /* ===== 1. 壁纸修复（底部白边） ===== */
 (function(){
@@ -2779,3 +2777,7 @@ var st=document.createElement('style');
     window.openChatInfo = fo;
   }
 })();
+
+/* 暗度归零 */
+if (+S.dim !== 0){ S.dim = 0; save(); }
+setTimeout(function(){ if (typeof applyWall === 'function') applyWall(); }, 400);
