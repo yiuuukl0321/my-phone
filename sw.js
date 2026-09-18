87dd17f1d5abeb2a887686f3f49dc7ce7792a542
yiuuukl0321 <krystalyiuu0321@gmail.com>
2026-09-18 20:32:33 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -69,17 +69,3 @@ self.addEventListener('fetch', e => {
 });
 
 
-self.addEventListener('push', function(e){
-  var d = {};
-  try { d = e.data.json(); } catch(err){}
-  e.waitUntil(self.registration.showNotification(d.title || '祁砚', {
-    body: d.body || '', tag: 'kai-proactive', data: { url: './' }
-  }));
-});
-self.addEventListener('notificationclick', function(e){
-  e.notification.close();
-  e.waitUntil(clients.matchAll({ type: 'window' }).then(function(l){
-    for (var i = 0; i < l.length; i++) if ('focus' in l[i]) return l[i].focus();
-    if (clients.openWindow) return clients.openWindow('./');
-  }));
-});
